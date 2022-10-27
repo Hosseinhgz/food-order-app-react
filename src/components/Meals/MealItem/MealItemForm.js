1d72e5e835bb005a166c09777f01dff709de620d
Hossein Hazratgholizadeh <hossein.hazratgholizade@gmail.com>
2022-10-27 23:36:57 +0300

    Create input field for meal items.

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import classes from './MealItemForm.module.css'
+import Input from "../../UI/Input";
 
 const MealItemForm = (props) => {
     return (
         <form className={classes.form}>
-            <input />
+            <Input
+                label="Amount"
+                input={{
+                    id:"number"+ props.id,
+                    type:'number',
+                    min:'0',
+                    max:'5',
+                    step:'1',
+                    defaultValue:'0',
+                }}
+            />
             <button >-</button>
             <button >+</button>
 
