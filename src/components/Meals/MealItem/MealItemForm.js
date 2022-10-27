eaf2cc393ec44eaead1a976a4212d2014b1588ea
Hossein Hazratgholizadeh <hossein.hazratgholizade@gmail.com>
2022-10-27 22:12:38 +0300

    Add input field to meal cards.

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import classes from './MealItemForm.module.css'
 
 const MealItemForm = (props) => {
     return (
-        <div>
-
-        </div>
+        <form className={classes.form}>
+            <input />
+            <button >Add</button>
+        </form>
     );
 };
 
